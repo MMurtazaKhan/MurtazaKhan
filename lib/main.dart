ce192c98488f02dcd10fcb85771448d11be05723
MMurtazakhan <murtaza10dec@gmail.com>
2021-05-07 16:43:29 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text("Muhammad Murtaza Khan    Email: murtaza10dec@gmail.com"),
+      ),
+    );
+  }
+}
